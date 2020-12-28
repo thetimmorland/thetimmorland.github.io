1334375811a6a26612c7571fc9b96cd63ed4ebd3
Timothy Morland <me@timothy-morland.com>
2020-12-28 13:24:29 -0500

    fix mobile min-size issue

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,23 @@ import { MDXProvider } from "@mdx-js/react";
 import NavBar from "../components/Navbar";
 import components from "../components/mdx";
 
+import { useEffect } from "react";
+
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener("resize", setVh);
+
+    return () => {
+      window.removeEventListener(setVh);
+    };
+  }, []);
+
   return (
     <div className="root">
       <NavBar />
