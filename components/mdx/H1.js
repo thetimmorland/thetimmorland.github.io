d5d6fed35866887d5e43dadec12f6f657e1e0c00
Timothy Morland <me@timothy-morland.com>
2020-12-27 19:54:59 -0500

    add more whitespace around page title

--- components/mdx/H1.js
+++ components/mdx/H1.js
@@ -1,7 +1,7 @@
 import H2 from "./H2";
 
 const H1 = ({ className, ...props }) => (
-  <h1 className="text-4xl mt-4 text-center" {...props} />
+  <h1 className="text-4xl p-4 text-center" {...props} />
 );
 
 export default H1;
