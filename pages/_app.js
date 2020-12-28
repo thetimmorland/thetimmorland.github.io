40855ebfb625d9fc5aadea3c53a31290728f6ada
Timothy Morland <me@timothy-morland.com>
2020-12-28 16:29:12 -0500

    reorganize layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,36 @@ import "../styles/globals.css";
 
 import { MDXProvider } from "@mdx-js/react";
 
+import CodeBlock from "../components/CodeBlock";
+import H1 from "../components/H1";
+import H2 from "../components/H2";
+import H3 from "../components/H3";
+import InlineCode from "../components/InlineCode";
+import Link from "../components/Link";
 import NavBar from "../components/Navbar";
-import components from "../components/mdx";
+import OrderedList from "../components/OrderedList";
+import Text from "../components/Text";
+import UnorderedList from "../components/UnorderedList";
+import Wrapper from "../components/Wrapper";
+import ResizeWatcher from "../components/ResizeWatcher";
 
-import { useEffect } from "react";
+const components = {
+  wrapper: Wrapper,
+  h1: H1,
+  h2: H2,
+  H3: H3,
+  p: Text,
+  a: Link,
+  code: CodeBlock,
+  inlineCode: InlineCode,
+  ol: OrderedList,
+  ul: UnorderedList,
+};
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    setVh();
-    window.addEventListener("resize", setVh);
-
-    return () => {
-      window.removeEventListener(setVh);
-    };
-  }, []);
-
   return (
     <div className="root">
+      <ResizeWatcher />
       <NavBar />
       <main>
         <MDXProvider components={components}>
